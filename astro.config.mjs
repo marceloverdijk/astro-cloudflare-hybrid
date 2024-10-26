c36f1219740c38a25c2bb00fa7b661f5462c09e8
Marcel Overdijk <marceloverdijk@gmail.com>
2024-10-26 11:32:35 +0200

    Excluding Prisma

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    ssr: {
+      // Example: Force a broken package to skip SSR processing, if needed
+      external: ['@prisma/client'],
+    },
+  },
 });
